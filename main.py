7c51fac048c93bb600cc328d7347828fbf02e7f0
Dev Varma <devtronvarma@gmail.com>
2021-11-13 20:19:28 -0600

    added placeholders for needed code

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-# define some key variables
+# define some key initial variables
 user_bankroll = 1000
 cpu_bankroll = 1000
-blind = 5
+blind_small = 5
+blind_big = 10
 
+# define the deck of cards
+
+
+# define the hierarchy of possible hands
+
+
+# functions for laying out the community cards -- flop, turn, river
+
+
+# function for wagering
+
+
+# function for determining cpu's hand
+
+
+# function for determining User's hand
+
+
+# function for determining winning hand
+
+
+# function for incrementing, decrementing bankrolls
+
+
+# define User class
